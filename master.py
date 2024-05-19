140a0c2f867e10da77922941788be5bf4af93d72
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2024-05-18 23:26:40 -0600

    .

--- master.py
+++ master.py
@@ -1,6 +1,6 @@
 import tableManager
 
-c = input('Lil Base@local: ')
+c = input('Tini-Base@local: ')
 c = c.replace(',','').split(' ')
 if c[0].lower() == 'create':
     print(tableManager.createTable(c[1:]))
